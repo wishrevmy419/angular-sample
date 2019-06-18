e5add25cb830d7b76bd6bed44d75dd1fdb22c34a
佑矢 <ymatsuhashi@matsuhashiyuuyanoMacBook-puro.local>
2019-06-18 13:55:04 +0900

    一覧画面にてマウスオーバーで詳細ボタンを表示させる

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../shared/models/product';
 import { ProductService } from '../../shared/services/product.service';
 
+class ProductListElement extends Product {
+  hovered: boolean;
+}
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -16,7 +20,14 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit() {
     this.productService.list().subscribe((products: Product[]) => {
-      this.products = products;
+      this.products = products.map((product: Product) => {
+        return {
+          ... product,
+          hovered: false,
+        };
+      });
     });
   }
+  hovered(product: ProductListElement): void { product.hovered = true; }
+  unhovered(product: ProductListElement): void { product.hovered = false; }
 }
